5381fb29c8be39a244b2550495d533c3746a2603
Shuhei Taku <101714586+ShuheiTaku@users.noreply.github.com>
2023-03-20 11:03:02 +0900

    Update and rename PolicyValueNetwork to PolicyValueNetwork.py

---
+++ outeshogi/network/PolicyValueNetwork.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from cshogi import *
+
+# 移動方向を表す定数
+MOVE_DIRECTION = [
+    UP, UP_LEFT, UP_RIGHT, LEFT, RIGHT, DOWN, DOWN_LEFT, DOWN_RIGHT,
+    UP2_LEFT, UP2_RIGHT,
+    UP_PROMOTE, UP_LEFT_PROMOTE, UP_RIGHT_PROMOTE, LEFT_PROMOTE, RIGHT_PROMOTE, DOWN_PROMOTE, DOWN_LEFT_PROMOTE, DOWN_RIGHT_PROMOTE,
+    UP2_LEFT_PROMOTE, UP2_RIGHT_PROMOTE
+] = range(20)
+# 入力特徴量の数
+FEATURES_NUM = len(PIECE_TYPES) * 2 + sum(MAX_PIECES_IN_HAND) * 2
+# 移動を表すラベルの数
+MOVE_PLANES_NUM = len(MOVE_DIRECTION) + len(HAND_PIECES)
+MOVE_LABELS_NUM = MOVE_PLANES_NUM * 81
+
+class Bias(nn.Module):
+    '''
+    方策に加えるためのバイアス
+    '''
+    def __init__(self, shape):
+        super(Bias, self).__init__()
+        self.bias=nn.Parameter(torch.zeros(shape))
+
+    def forward(self, input):
+        return input + self.bias
+
+class ResNetBlock(nn.Module):
+    '''
+    このブロックを積み重ねる
+    '''
+    def __init__(self, channels):
+        super(ResNetBlock, self).__init__()
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(channels)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        return F.relu(out + x)
+
+class PolicyValueNetwork(nn.Module):
+    '''
+    これが本体。方策と評価値の2つを同時に解くマルチタスク構造になっている。
+    '''
+    def __init__(self, blocks=10, channels=192, fcl=256):
+        super(PolicyValueNetwork, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=FEATURES_NUM, out_channels=channels, kernel_size=3, padding=1, bias=False)
+        self.norm1 = nn.BatchNorm2d(channels)
+
+        # resnet blocks
+        self.blocks = nn.Sequential(*[ResNetBlock(channels) for _ in range(blocks)])
+
+        # policy head
+        self.policy_conv = nn.Conv2d(in_channels=channels, out_channels=MOVE_PLANES_NUM, kernel_size=1, bias=False)
+        self.policy_bias = Bias(MOVE_LABELS_NUM)
+
+        # value head
+        self.value_conv1 = nn.Conv2d(in_channels=channels, out_channels=MOVE_PLANES_NUM, kernel_size=1, bias=False)
+        self.value_norm1 = nn.BatchNorm2d(MOVE_PLANES_NUM)
+        self.value_fc1 = nn.Linear(MOVE_LABELS_NUM, fcl)
+        self.value_fc2 = nn.Linear(fcl, 1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(self.norm1(x))
+
+        # resnet blocks
+        x = self.blocks(x)
+
+        # policy head
+        policy = self.policy_conv(x)
+        policy = self.policy_bias(torch.flatten(policy, 1))
+
+        # value head
+        value = F.relu(self.value_norm1(self.value_conv1(x)))
+        value = F.relu(self.value_fc1(torch.flatten(value, 1)))
+        value = self.value_fc2(value)
+
+        return policy, value
